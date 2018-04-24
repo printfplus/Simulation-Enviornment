69b309e220d701c9d1c55b3b9fc55f6718663aac
printfplus <printfplus@gmail.com>
2018-04-24 14:41:20 +0800

    2D plot with random over

--- SimuEnv.py
+++ SimuEnv.py
@@ -4,6 +4,8 @@ import json
 from car_follow import *
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import time
 
 car_follow_Dic = {
     0: "None",
@@ -11,6 +13,8 @@ car_follow_Dic = {
     2: "LEFT",
     3: "RIGHT"
 }
+
+Test_Step = 100
 
 class SimuEnv():
 	"""docstring for SimuEnv"""
@@ -57,12 +61,14 @@ class SimuEnv():
 		self.initial.set_state(range_error,range_rate_error)
 
 
-	def simulate(self):
+	def simulate(self,random = 'off'):
 		step = self.get_step()
 		x1,v1,x2,v2 = self.initial.get_state()
 		for i in range(step):
-			#print(i)
-			self.a_back_series[i] = pid_follow(x1,v1,x2,v2)
+			if random == 'off':
+				self.a_back_series[i] = pid_follow(x1,v1,x2,v2)
+			else:
+				self.a_back_series[i] = pid_follow_with_random(x1,v1,x2,v2)
 			x1,v1 = update(x1,v1,self.get_a1(i))
 			x2,v2 = update(x2,v2,self.a_back_series[i])
 			if x1 - x2 < 2:
@@ -73,22 +79,41 @@ class SimuEnv():
 
 
 
-	def start(self,rlb,rub,rstep,rrlb,rrub,rrstep):
+	def start(self,rlb,rub,rstep,rrlb,rrub,rrstep,random = 'off'):
+		print("starting simulation")
+		tic = time.time()
+
 		range_error_series = get_series(rlb,rub,rstep)
-		print(range_error_series)
+		#print(range_error_series)
 		range_rate_error_series = get_series(rrlb,rrub,rrstep)
-		print(range_rate_error_series)
+		#print(range_rate_error_series)
 		whole_serie = []
-		for RE in range(rstep):
+		for RE in range(len(range_error_series)):
+			print(RE)
 			RRE_serie = []
-			for RRE in range(rrstep):
-				self.set_state(range_error_series[RE],range_rate_error_series[RRE])
-				self.simulate()
-				RRE_serie.append(int(self.collision_flag))
+			for RRE in range(len(range_rate_error_series)):
+				if random == 'off':
+					self.set_state(range_error_series[RE],range_rate_error_series[RRE])
+					self.simulate()
+					RRE_serie.append(int(self.collision_flag))
+				else:
+					collision_times = 0
+					for i in range(Test_Step):
+						self.set_state(range_error_series[RE],range_rate_error_series[RRE])
+						self.simulate(random)
+						collision_times = collision_times+self.collision_flag
+					self.collision_rate = collision_times/Test_Step
+					RRE_serie.append(self.collision_rate)		
 			whole_serie.append(RRE_serie)
 
-		serie = np.array(whole_serie)
-		return serie
+		df = pd.DataFrame(whole_serie,index=range_error_series,columns=range_rate_error_series)
+		
+		toc = time.time()
+
+		return df,toc-tic
+
+	def plot(self):
+		plt.plot()
 
 
 
@@ -105,5 +130,8 @@ class SimuEnv():
 I = Initial()
 env = SimuEnv(initial = I)
 env.setting(1)
-whole_serie = env.start(rlb = 0,rub = 100,rstep = 50,rrlb = -20,rrub = 20,rrstep = 50)
-np.savetxt('new.csv', whole_serie, delimiter = ',')  
+whole_serie,whole_time = env.start(rlb = 0,rub = 100,rstep = 50,rrlb = -20,rrub = 20,rrstep = 40,random = 'on')
+whole_serie.to_csv('new.csv')
+#print(whole_serie)
+print("using "+str(whole_time)+" seconds")
+
